86326264ffc6d4f5826c56f692ebe29533241213
Taylor johnson <taylorjohnsondev@gmail.com>
2023-04-06 04:18:52 -0400

    fixed some issues

--- capstone/src/axios/axiosConfig.js
+++ capstone/src/axios/axiosConfig.js
@@ -2,7 +2,7 @@ import { API_URL } from "constants.js";
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: API_URL || "http://localhost:3001/", 
+  baseURL: API_URL || "/", 
   headers: { "Content-Type": "application/json" },
 });
 
