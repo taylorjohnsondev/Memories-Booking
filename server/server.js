a30f9624328a149d519a3f2d19d21a85a2e0aea9
Taylor johnson <taylorjohnsondev@gmail.com>
2023-04-07 13:51:18 -0400

    reverted server.js changes

--- server/server.js
+++ server/server.js
@@ -9,25 +9,27 @@ const path = require("path");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+app.use;
 
-if (process.env.NODE_ENV === "production") {
+app.use("/", require("./routes/index"));
+if (process.env.NODE_ENV === "production") { 
   app.use(express.static(path.join(__dirname, "../capstone/build")));
   app.all("*", (req, res, next) => {
     res.sendFile(path.resolve(__dirname, "../capstone/build/index.html"));
   });
 }
-app.use("/", require("./routes/index"));
 
 const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@capstone-group-project.1yvi8j1.mongodb.net/?retryWrites=true&w=majority`;
 
 async function connectDB() {
-  try {
+  try { 
     await mongoose.connect(url);
     console.log("Connected to Database");
   } catch (error) {
     console.error(error);
   }
 }
+
 connectDB();
 
 app.listen(port, () => {
